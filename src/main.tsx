f688c912e1eee8bec28b0e140e79b7a1d1629541
Roger Gomez <gomez.roger779@gmail.com>
2025-02-19 22:19:52 -0400

    feature: installed Work OS AuthKit

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { Suspense, StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { AuthKitProvider } from '@workos-inc/authkit-react';
 
 import App from './app';
 
@@ -11,12 +12,17 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <Suspense>
-          <App />
-        </Suspense>
-      </BrowserRouter>
-    </HelmetProvider>
+    <AuthKitProvider
+      clientId="client_01JMGG6DWRZDBVW1H2Y0JD97YZ"
+      redirectUri="http://localhost:3039/"
+    >
+      <HelmetProvider>
+        <BrowserRouter>
+          <Suspense>
+            <App />
+          </Suspense>
+        </BrowserRouter>
+      </HelmetProvider>
+    </AuthKitProvider>
   </StrictMode>
 );
